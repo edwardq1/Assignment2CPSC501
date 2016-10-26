b735b65be43cb5a75a5b29cb24b50731558a28cf
edwardq1 <equach@ucalgary.ca>
2016-10-26 14:13:34 -0600

    refactor
    Refactor by Long Method:
    Reduced the method inspector by creating several methods that handle
    different jobs such as obtaining interfaces, obtaining constructors,
    etc.

--- Inspector.java
+++ Inspector.java
@@ -3,6 +3,15 @@ import java.util.Vector;
 public class Inspector {
 	public void inspect(Object obj, boolean recursive){
 		Class classObject = obj.getClass();
+		obtainClass(classObject);
+		obtainInterfaces(classObject);
+		obtainMethods(classObject);
+		obtainConstructors(classObject);
+		obtainFields(classObject, obj);
+		
+	}
+	
+	public void obtainClass(Class classObject){
 		String className = classObject.getName();
 		// print the declaring class
 		System.out.println("Declaring class: " + className);
@@ -11,14 +20,20 @@ public class Inspector {
 		className = superClass.getName();
 		System.out.println("Super class: " + className);
 
+	}
+	
+	public void obtainInterfaces(Class classObject){
 		// obtaining the interfaces
 		Class[] interfaces = classObject.getInterfaces();
 		System.out.print("Interfaces:");
 		for (int i = 0; i < interfaces.length; i++){
 			System.out.print(" " + interfaces[i].getName() + ",");
 		}
+	}
+	
+	//Method that will handle grabbing methods of a class
+	public void obtainMethods(Class classObject){
 		System.out.println("");
-		
 		Method[] classMethods = classObject.getDeclaredMethods();
 		// information for a method
 		System.out.println("Class methods:");
@@ -35,48 +50,44 @@ public class Inspector {
 			System.out.print("\n		Return type: " + method.getReturnType());
 			int temp = method.getModifiers();
 			System.out.println("\n		Modifier: " + Modifier.toString(temp));
+			System.out.println("");
 
 		}
-		System.out.println("");
-		
-		// Constructors
-		Constructor[] constructors = classObject.getConstructors();
-		System.out.println("Constructors:");
-		for (Constructor constructor : constructors){
-			int temp = constructor.getModifiers();
-			System.out.print("		" + constructor.getName() + "\n");
-			Class[] parameterList = constructor.getParameterTypes();
-			System.out.print("			Parameters:");
-			for(Class parameter : parameterList)
-				System.out.print(" " + parameter.getName() + ", ");
-			System.out.print("\n			Modifier: " + Modifier.toString(temp));
-			System.out.println();
-		}
-
+	}
+	
+	//Method that will handle grabbing the fields of a class
+	public void obtainFields(Class classObject, Object object){
 		//obtain fields
-		
 		Field[] classFields = classObject.getDeclaredFields();
 		System.out.println("Fields:");
 		for(Field field : classFields){
 			int temp = field.getModifiers();
-			System.out.print("		" + Modifier.toString(temp)
-					+ " " + field.getType() + " "+ field.getName() + ": ");
+			System.out.print("	" + Modifier.toString(temp)
+					+ " " + field.getType() + " "+ field.getName() + " = ");
 			field.setAccessible(true);
 			try {
-				System.out.print(field.get(obj)+ "\n");
+				System.out.print(field.get(object)+ "\n");
 			} catch (IllegalArgumentException | IllegalAccessException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			// supposed to recurse but it doesnt work
-
 		}
-
-		
-		// handle hiearchy
-		/*
-		if (classObject.getSuperclass() != null){
-			inspect(classObject.getSuperclass(), false);
-		}*/
+	}
+	//Method that will handle grabbing the constructors of a class
+	public void obtainConstructors(Class classObject){
+		// Constructors
+		Constructor[] constructors = classObject.getConstructors();
+		System.out.println("Constructors:");
+		for (Constructor constructor : constructors){
+			int temp = constructor.getModifiers();
+			System.out.print("	" + constructor.getName() + ":\n");
+			Class[] parameterList = constructor.getParameterTypes();
+			System.out.print("		Parameters:");
+			for(Class parameter : parameterList)
+				System.out.print(" " + parameter.getName() + ", ");
+			System.out.print("\n		Modifier: " + Modifier.toString(temp));
+			System.out.println();
+		}
 	}
 }

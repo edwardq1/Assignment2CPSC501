2f6db8a42f6a9e0a8a6b5a99dfad8d185d0e741d
edwardq1 <equach@ucalgary.ca>
2016-10-25 19:03:16 -0600

    Added some functionality for assignment
    Able to obtain the declaring class, superclass, the interfaces the class
    implements, methods and all the requiremets for it.

--- Inspector.java
+++ Inspector.java
@@ -1,7 +1,38 @@
 import java.lang.reflect.*;
 public class Inspector {
 	public void inspect(Object obj, boolean recursive){
-		obj = new Object();
-		System.out.println(obj.getClass());
+		Class classObject = obj.getClass();
+		String className = classObject.getName();
+		// print the declaring class
+		System.out.println("Declaring class: " + className);
+		
+		Class superClass = classObject.getSuperclass();
+		className = superClass.getName();
+		System.out.println("Super class: " + className);
+
+		// obtaining the interfaces
+		Class[] interfaces = classObject.getInterfaces();
+		System.out.print("Interfaces:");
+		for (int i = 0; i < interfaces.length; i++){
+			System.out.print(" " + interfaces[i].getName() + ",");
+		}
+		System.out.println("");
+		
+		Method[] classMethods = classObject.getMethods();
+		System.out.println("Class methods:");
+		for(Method method : classMethods){
+			System.out.println(method.getName() + ": ");
+			Class[] methodParameters = method.getParameterTypes();
+			for (Class parameter : methodParameters)
+				System.out.print(" " + parameter.getName() + ",");
+			Class[] methodException = method.getExceptionTypes();
+			for (Class exception : methodException)
+				System.out.print(" " + exception.getName() + ",");
+			System.out.print(" " + method.getReturnType() + ",");
+			System.out.println(" " + method.getModifiers() + "");
+
+		}
+		System.out.println("");
+
 	}
 }

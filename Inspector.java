8fc5acbf91737d65e4a42c3d750c7a6d0e7a5ecb
edwardq1 <equach@ucalgary.ca>
2016-10-27 20:24:51 -0600

    Completed
    Finished traversing through a Hiearchy and finished going into each
    field object.
    Only thing that doesnt work is when you pass in an array of objects.

--- Inspector.java
+++ Inspector.java
@@ -1,9 +1,14 @@
+import java.util.*;
 import java.lang.reflect.*;
+import java.util.ArrayList;
 import java.util.Vector;
 public class Inspector {
+
 	public void inspect(Object obj, boolean recursive){
+		List<Field> toDoList = new ArrayList();
 		Class classObject = obj.getClass();
 		Class superClass = classObject.getSuperclass();
+
 		if (classObject.isArray()){
 			System.out.println("THIS IS AN ARRAY OF CLASSES");
 		}
@@ -12,13 +17,65 @@ public class Inspector {
 			inspectInterfaces(classObject);
 			inspectMethods(classObject);
 			inspectConstructors(classObject);
-			inspectClassFields(classObject, obj, recursive);
+			inspectClassFields(classObject, obj, recursive, toDoList);
 		}
-	/*	if ((superClass != null) && recursive == true){
+		if (!toDoList.isEmpty() && recursive){
+			Field currentField;
+			System.out.println("******************Printing field information******************");
+			for (int i = 0; i< toDoList.size(); i++){
+				currentField = toDoList.get(i);
+				System.out.println("******************Field Name: "+ currentField.getName() +"******************");
+				try {
+					inspect(currentField.get(obj), currentField.get(obj).getClass(), recursive);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			toDoList.clear();
+		}
+		if ((superClass != null) && (!superClass.isInstance(Object.class))){
 			System.out.println("******************Entering superclass******************");
-			inspect(classObject.getSuperclass(), false);
-		}*/
-		
+			inspect(obj, classObject.getSuperclass(), recursive);
+		}
+
+	}
+	//This is the recursive inspect that gets called
+	//Takes in 3 parameters
+	public void inspect(Object obj, Class c, boolean recursive){
+		List<Field> toDoList = new ArrayList();
+		Class superClass = c.getSuperclass();
+		if (c.isArray()){
+			System.out.println("THIS IS AN ARRAY OF CLASSES");
+		}
+		else{
+			inspectClass(c);
+			inspectInterfaces(c);
+			inspectMethods(c);
+			inspectConstructors(c);
+			inspectClassFields(c, obj, recursive, toDoList);
+		}
+		if (!toDoList.isEmpty() && recursive){
+			Field currentField;
+			System.out.println("******************Printing field information******************");
+			for (int i = 0; i< toDoList.size(); i++){
+				currentField = toDoList.get(i);
+				System.out.println("******************Field Name: "+ currentField.getName() +"******************");
+				try {
+					inspect(currentField.get(obj), currentField.get(obj).getClass(), recursive);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			toDoList.clear();
+		}
+		if ((superClass != null) && (!superClass.isInstance(Object.class))){
+			System.out.println("******************Entering superclass******************");
+			inspect(obj, c.getSuperclass(), recursive);
+		}
+
+
 	}
 	//Inspect a class by getting its declaring class and superclass
 	public void inspectClass(Class classObject){
@@ -60,24 +117,27 @@ public class Inspector {
 			}
 			else
 				System.out.print(" No parameters");
-			System.out.print("\n		Exceptions: ");
+			System.out.println();
+			System.out.print("		Exceptions: ");
 			Class[] methodException = method.getExceptionTypes();
 			if (methodException.length != 0){
 				for (Class exception : methodException)
-					System.out.print(" " + exception.getSimpleName());
+					System.out.print(" " + exception.getName());
 			}
 			else
 				System.out.print(" No exceptions");
-			System.out.print("\n		Return type: " + method.getReturnType());
+			System.out.println();
+			System.out.print("		Return type: " + method.getReturnType());
+			System.out.println();
 			int temp = method.getModifiers();
-			System.out.println("\n		Modifier: " + Modifier.toString(temp));
+			System.out.println("		Modifier: " + Modifier.toString(temp));
 			System.out.println("");
 
 		}
 	}
 	
 	//Method that will handle grabbing the fields of a class
-	public void inspectClassFields(Class classObject, Object object, Boolean recursive){
+	public void inspectClassFields(Class classObject, Object object, Boolean recursive, List<Field> l){
 		//obtain fields
 		Object o = null;
 		Field[] classFields = classObject.getDeclaredFields();
@@ -90,14 +150,16 @@ public class Inspector {
 			} catch (IllegalArgumentException | IllegalAccessException e1) {
 				e1.printStackTrace();
 			}
-			inspectField(field, o, isObject, recursive, object);
+			inspectField(field, o, isObject, recursive, object, l);
 			// supposed to recurse but it doesnt work
 		}
 	}
-	public void inspectField(Field field, Object o, boolean isObject, boolean recursive, Object object){
+	public void inspectField(Field field, Object o, boolean isObject, boolean recursive, Object object, List<Field> l ){
 		if (field.getType().isArray() && o != null){
-			System.out.println("	Array field: " + "\n		Component type: " + field.getType().getComponentType() + "\n		Name: " + 
-					field.getName() + "\n		Length: " + Array.getLength(o));
+			System.out.println("	Array field: ");
+			System.out.println("		Component type: " + field.getType().getComponentType());
+			System.out.println("		Name: " + field.getName());
+			System.out.println("		Length: " + Array.getLength(o));
 			
 			System.out.print("		Contents: ");
 			for (int i = 0; i < Array.getLength(o); i++){
@@ -105,12 +167,18 @@ public class Inspector {
 			}
 		}
 		else if ((!isObject) && recursive == true && o != null){
-			System.out.println("******************Entering object******************");
-			inspect(field.getType().getName(), false);
+			try {
+				l.add(field);
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//System.out.println("******************Entering object******************");
+			//inspect(field.getType().getName(), false);
 		}
 		else if ((!isObject) && recursive == false && o != null)
 			try {
-				System.out.print("	" + field.getType().getSimpleName() + System.identityHashCode(field.get(object)));
+				System.out.print("	Reference value of object: " + field.getType().getSimpleName() + System.identityHashCode(field.get(object)));
 			} catch (IllegalArgumentException | IllegalAccessException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -137,8 +205,9 @@ public class Inspector {
 		System.out.println("Constructors:");
 		for (Constructor constructor : constructors){
 			int temp = constructor.getModifiers();
-			System.out.print("	" + constructor.getName() + ":\n");
+			System.out.print("	" + constructor.getName() + ":");
 			Class[] parameterList = constructor.getParameterTypes();
+			System.out.println();
 			System.out.print("		Parameters:");
 			if (parameterList.length != 0){
 				for(Class parameter : parameterList)
@@ -146,7 +215,8 @@ public class Inspector {
 			}
 			else
 				System.out.print(" No parameters");
-			System.out.print("\n		Modifier: " + Modifier.toString(temp));
+			System.out.println();
+			System.out.print("		Modifier: " + Modifier.toString(temp));
 			System.out.println();
 		}
 	}
